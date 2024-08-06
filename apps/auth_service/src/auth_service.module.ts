af8bd34ba51ebdc21d1430ff4a758f396d59d53b
The-good-guyy <80090032+The-good-guyy@users.noreply.github.com>
2024-08-07 01:17:18 +0700

    commit

--- apps/auth_service/src/auth_service.module.ts
+++ apps/auth_service/src/auth_service.module.ts
@@ -5,19 +5,23 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import type { RedisClientOptions } from 'redis';
 import * as redisStore from 'cache-manager-redis-store';
+
+// to integrate redis and cache-manager, now install `pnpm install cache-manager-redis-store@^2.0.0`
+// Don't update cache manager , redis only compatible with cache-manager version 2.0.0
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: ['./env/postgres.env', './env/jwt.env'],
-    }),
-    CacheModule.register({
+    CacheModule.register<RedisClientOptions>({
       store: redisStore,
       socket: {
         host: 'localhost',
         port: 6379,
       },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['./env/postgres.env', './env/jwt.env'],
     }),
     DatabaseModule,
     AuthModule,
