ec9972f60de4407a8d26edd64d3f5fe4d4d6286a
The-good-guyy <80090032+The-good-guyy@users.noreply.github.com>
2024-08-07 11:40:53 +0700

    commit

--- apps/auth_service/src/auth/auth.controller.ts
+++ apps/auth_service/src/auth/auth.controller.ts
@@ -5,8 +5,62 @@ import { createUserDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Post()
+  @Post('signin/local')
   signInLocal(@Body() createUserDto: createUserDto) {
     return this.authService.signInLocal(createUserDto);
   }
 }
+
+// import {
+//   Controller,
+//   Post,
+//   Body,
+//   HttpCode,
+//   HttpStatus,
+//   UseGuards,
+//   Req,
+//   Res,
+// } from '@nestjs/common';
+// import { Response } from 'express';
+// import { AuthService } from './auth.service';
+// import { AuthDto } from './dto/auth.dto';
+// import { Tokens } from './types';
+// import { AtGuard, RtGuard } from 'src/common/guard';
+// import { GetCurrentUser } from 'src/common/decorators';
+
+// @Controller('auth')
+// export class AuthController {
+//   constructor(private authService: AuthService) {}
+//   @Post('local/signup')
+//   @HttpCode(HttpStatus.CREATED)
+//   signupLocal(
+//     @Body() authDto: AuthDto,
+//     @Res({ passthrough: true }) response: Response,
+//   ): Promise<Tokens> {
+//     return this.authService.signupLocal(authDto, response);
+//   }
+
+//   @Post('local/sigin')
+//   @HttpCode(HttpStatus.OK)
+//   siginLocal(@Body() authDto: AuthDto): Promise<Tokens> {
+//     return this.authService.signinLocal(authDto);
+//   }
+
+//   @UseGuards(AtGuard)
+//   @Post('/logout')
+//   @HttpCode(HttpStatus.OK)
+//   logout(@GetCurrentUser('sub') userId: number): Promise<boolean> {
+//     return this.authService.logout(userId);
+//   }
+
+//   @UseGuards(RtGuard)
+//   @Post('/refresh')
+//   @HttpCode(HttpStatus.OK)
+//   refreshTokens(
+//     @GetCurrentUser('sub') userId: number,
+//     @GetCurrentUser('refreshToken') refreshToken: string,
+//   ): Promise<Tokens> {
+//     // console.log(refreshToken);
+//     return this.authService.refreshTokens(userId, refreshToken);
+//   }
+// }
--- apps/auth_service/src/auth/auth.service.ts
+++ apps/auth_service/src/auth/auth.service.ts
@@ -1,18 +1,23 @@
 import {
-  ForbiddenException,
   Injectable,
   UnprocessableEntityException,
+  Inject,
+  ForbiddenException,
 } from '@nestjs/common';
-import { createUserDto } from './dto';
+import { createUserDto, loginUserDto } from './dto';
 import { EntityManager, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { User } from './entities/user.entity';
 import { ConfigService } from '@nestjs/config';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import { Tokens } from './types';
 @Injectable()
 export class AuthService {
   constructor(
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
     @InjectRepository(User)
     private usersRepository: Repository<User>,
     private entityManager: EntityManager,
@@ -23,17 +28,18 @@ export class AuthService {
     return bcrypt.hash(data, 10);
   }
   async updateRtHash(userId: string, rt: string) {
-    const hash = await this.hashData(rt);
-    this.usersRepository.update({ uuid: userId }, { password: hash });
+    this.cacheManager.set(userId, rt, {
+      ttl: 60 * 60 * 24 * 27,
+    });
   }
-  async getTokens(userId: number, email: string) {
+  async getTokens(userId: string, email: string, role: string) {
     const [at, rt] = await Promise.all([
       this.jwtService.signAsync(
-        { sub: userId, email },
+        { sub: userId, email, role },
         { secret: this.config.get<string>('AT_SECRET'), expiresIn: 60 * 15 },
       ),
       this.jwtService.signAsync(
-        { sub: userId, email },
+        { sub: userId, email, role },
         {
           secret: this.config.get<string>('RT_SECRET'),
           expiresIn: 60 * 60 * 24 * 27,
@@ -51,11 +57,50 @@ export class AuthService {
     });
     if (user) throw new UnprocessableEntityException('Email is already used');
     const hash = await this.hashData(createUserDto.password);
-    const newUser = this.usersRepository.create({
+    let newUser = this.usersRepository.create({
       ...createUserDto,
       password: hash,
       createdAt: new Date(),
     });
-    return this.usersRepository.save(newUser);
+    newUser = await this.usersRepository.save(newUser);
+    const tokens = await this.getTokens(
+      newUser.uuid,
+      newUser.email,
+      newUser.role,
+    );
+    this.updateRtHash(newUser.uuid, tokens.refresh_token);
+    return tokens;
+  }
+  async signinLocal(loginUserDto: loginUserDto): Promise<Tokens> {
+    const user = await this.usersRepository.findOne({
+      where: { email: loginUserDto.email },
+    });
+    if (!user) throw new ForbiddenException('Access Denied');
+    const passwordMatches = await bcrypt.compare(
+      loginUserDto.password,
+      user.password,
+    );
+    if (!passwordMatches) throw new ForbiddenException('Access Denied');
+    const tokens = await this.getTokens(user.uuid, user.email, user.role);
+    await this.updateRtHash(user.uuid, tokens.refresh_token);
+    return tokens;
+  }
+  async logout(userId: number): Promise<boolean> {
+    this.cacheManager.del(userId);
+    return true;
+  }
+  async refreshTokens(userId: string, rt: string) {
+    const user = await this.usersRepository.findOne({
+      where: { uuid: userId },
+    });
+    if (!user) throw new ForbiddenException('Access Denied');
+    const cachedItem = await this.cacheManager.get(userId);
+    if (!cachedItem && cachedItem !== rt) {
+      this.cacheManager.del(userId);
+      throw new ForbiddenException('Access Denied');
+    }
+    const tokens = await this.getTokens(user.uuid, user.email, user.role);
+    await this.updateRtHash(user.uuid, tokens.refresh_token);
+    return tokens;
   }
 }
