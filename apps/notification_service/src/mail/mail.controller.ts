7797990329a9513cd373a6027dd81abc4e632cdc
lapduy1112 <lapduy111201@gmail.com>
2024-08-21 15:56:53 +0700

    update

--- apps/notification_service/src/mail/mail.controller.ts
+++ apps/notification_service/src/mail/mail.controller.ts
@@ -1,16 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { User } from 'apps/notification_service/src/mail/user.entity';
 @Controller()
 export class MailController {
   constructor(private mailService: MailService) {}
-
+  generateToken(): string {
+    return Math.random().toString(36).substring(2, 15);
+  }
   @MessagePattern('send-confirmation-email')
   async sendUserConfirmation(@Payload() data: any) {
     const { email, name } = data;
 
     try {
-      await this.mailService.sendUserConfirmation(email, name);
+      const token = this.generateToken();
+      const user: User = { email, name };
+      await this.mailService.sendUserConfirmation(user, token);
       return { message: 'Confirmation email sent successfully' };
     } catch (error) {
       return {
