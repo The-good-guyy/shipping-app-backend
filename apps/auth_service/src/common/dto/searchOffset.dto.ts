01923924b11478ddc46917dbaa80b6f0e9291af2
The-good-guyy <80090032+The-good-guyy@users.noreply.github.com>
2024-08-25 22:15:00 +0700

    Update searchOffset.dto.ts

--- apps/auth_service/src/common/dto/searchOffset.dto.ts
+++ apps/auth_service/src/common/dto/searchOffset.dto.ts
@@ -1,12 +1,3 @@
-// import { Field, InputType } from '@nestjs/graphql';
-// import { SearchOffsetPaginationDto } from '@common/searchOffsetPagination.dto';
-// import { EVesselOrderBy } from '../vessel.enum';
-
-// @InputType()
-// export class SearchOffsetVesselDto extends SearchOffsetPaginationDto {
-//   @Field(() => EVesselOrderBy)
-//   orderBy?: EVesselOrderBy = EVesselOrderBy.createdAt;
-// }
 import { OrderBy } from '../constants';
 import { SearchOffsetPaginationDto } from './searchOffsetPagination.dto';
 import { IsEnum, IsOptional } from 'class-validator';
